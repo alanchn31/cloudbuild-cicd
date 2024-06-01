8a313702266e3e5cd1cdf9db0bf5da5be5e2a820
Alan Choon <alanchn31@gmail.com>
2024-06-01 20:55:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 client = bigquery.Client()
 
 @app.route('/')
+
 def main(big_query_client=client):
     table_id = "gcp-mlops-425102.test_schema.stroke_data"
     job_config = bigquery.LoadJobConfig(
